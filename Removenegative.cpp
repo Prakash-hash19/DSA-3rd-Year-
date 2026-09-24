f78df324e2406ee76880645c3f194c835d0ebf2e
Prakash-hash19 <dinesh8889224006@gmail.com>
2026-09-25 05:13:39 +0530

    Update Removenegative.cpp

--- Removenegative.cpp
+++ Removenegative.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int A[] = {10, -5, 20, -3, 40, -8};
+    int n = 6;
+
+    cout << "[";
+
+    for (int i = 0; i < n; i++) {
+        if (A[i] >= 0) {
+            cout << A[i];
+
+            // Print comma except after the last positive number
+            bool hasNext = false;
+            for (int j = i + 1; j < n; j++) {
+                if (A[j] >= 0) {
+                    hasNext = true;
+                    break;
+                }
+            }
+            if (hasNext)
+                cout << ", ";
+        }
+    }
+
+    cout << "]";
+
+    return 0;
+}
